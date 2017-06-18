f8fba8ac3968e60714e9e3efcbad973a607db205
yasuhiro hashimoto <0084kaicyo@gmail.com>
2017-06-18 21:57:21 +0900

    バトルキャラクターを表現するクラスとメインクラスの作成(前者は未実装)

---
+++ src/BattleCharacter.kt
@@ -0,0 +1,11 @@
+/**
+ * Created by oohashi on 2017/06/18.
+ */
+class BattleCharacter {
+    var Hp:    Int get set
+    var Level: Int get set
+    constructor() {
+        Hp = 0
+        Level = 1
+    }
+}
---
+++ src/Main.kt
@@ -0,0 +1,7 @@
+/**
+ * Created by oohashi on 2017/06/18.
+ */
+fun main(args: Array<String>) {
+    var hoge = BattleCharacter()
+    println("Hp = ${hoge.Hp}\nLevel = ${hoge.Level}")
+}
