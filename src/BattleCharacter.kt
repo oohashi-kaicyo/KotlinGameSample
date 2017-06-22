299c6413d24912f6f543a033e320707b42371516
yasuhiro hashimoto <0084kaicyo@gmail.com>
2017-06-22 22:20:58 +0900

    書式の変更及び装備に関する記述を追加(未実装)

--- src/BattleCharacter.kt
+++ src/BattleCharacter.kt
@@ -1,15 +1,29 @@
+
+import sun.security.util.Length
+
 /**
  * Created by oohashi on 2017/06/18.
  */
-abstract class BattleCharacter: IAttack {
-    var hp:    Int get set
-    var level: Int get set
+abstract class BattleCharacter: IAttack, ICharacteristic, IAttackPoint {
+    var Hp:    Int get set
+    var Level: Int get set
+    var SelectedWeapon: IWeapon get set
+    override var Characteristics: Array<Characteristic> get set
+    override var AttackPoint: Int get set
+    // TODO: 装備用のスロットクラスの作成
     constructor() {
-        this.hp = 0
-        this.level = 1
+        Hp = 0
+        Level = 0
+        SelectedWeapon = null
+        Characteristics = arrayOf()
+        AttackPoint = 0
     }
     override fun attack(enemies: Array<BattleCharacter>) {
         val myself: BattleCharacter = this
         var damage: Damage = Damage(myself, enemies)
+    }
+    // TODO: バトルキャラに装備品を装備させる(スロットに装填)
+    fun Equip(slotIdx: Int, equipment: IEquipment) {
+        error("未実装")
     }
 }
